4fe930bb1eb45433df69f5c8131424143a0a6aa4
MNATorres <mtri21019@gmail.com>
2023-05-04 04:00:09 -0300

    handleAdd and updateList function is added

--- src/components/Chat/chatInput/ChatInput.tsx
+++ src/components/Chat/chatInput/ChatInput.tsx
@@ -1,18 +1,22 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import './ChatInput.css'
 import { IoMdSend } from 'react-icons/io';
 import { MdOutlineEmojiEmotions } from 'react-icons/md';
 import { BsFiletypeDoc } from 'react-icons/bs';
 import { FiImage } from 'react-icons/fi';
-
+import { ChatContext } from '../../../globalContext/ChatContext';
 
 
 export default function ChatInput() {
+    const { updateText, handleAdd } = useContext(ChatContext)
+
     return (
         <div className='inputContainer'>
             <div className='textContainer'>
-                <textarea placeholder='Aa'></textarea>
-                <IoMdSend className='iconSend' />
+                <textarea onChange={updateText} placeholder='Aa'></textarea>
+                <button onClick={handleAdd}>
+                    <IoMdSend className='iconSend' />
+                </button>
             </div>
             <div className="media-container">
                 <MdOutlineEmojiEmotions className="emojis" />
--- src/globalContext/ChatContext.tsx
+++ src/globalContext/ChatContext.tsx
@@ -8,12 +8,22 @@ type PropsMessages = {
     timestamp: string
 }
 
+type PropsInputText = {
+    id: number,
+    text: string,
+    timestamp: string,
+    send: string
+}
+
 interface ChatContext {
-    handleClose: () => void
-    isClose: boolean
-    messages: PropsMessages[]
-    currentUser: string
+    handleClose: () => void;
+    isClose: boolean;
+    messages: PropsMessages[];
+    currentUser: string;
     isCurrentUser: (sender: string) => boolean;
+    updateText: (e: React.BaseSyntheticEvent) => void;
+    handleAdd: () => void
+
 }
 
 export const ChatContext = React.createContext<ChatContext>({
@@ -21,13 +31,34 @@ export const ChatContext = React.createContext<ChatContext>({
     isClose: true,
     messages: [],
     currentUser: "",
-    isCurrentUser: () => true
+    isCurrentUser: () => true,
+    updateText: (e: React.BaseSyntheticEvent) => { },
+    handleAdd: () => { },
 })
 
 export const ChatProvider = ({ children }: { children: React.ReactNode }) => {
     const [isClose, setIsClose] = useState(true)
-    const [messages, setMessages] = useState<PropsMessages[]>([]);
+    const [messages, setMessages] = useState<PropsMessages[]>([])
     const [currentUser, setCurrentUser] = useState("Support")
+    const [text, setText] = useState("")
+    const [listChats, setListChats] = useState<PropsInputText[]>([])
+
+    const updateText = (e: React.BaseSyntheticEvent) => {
+        setText(e.currentTarget.value);
+        console.log(text)
+    }
+
+    const handleAdd = () => {
+        setListChats([...listChats, { id: Math.random(), text: text, timestamp: "10/12/2023", send: "Jhon" }]);
+        setText(() => "");
+    }
+
+    useEffect(() => {
+        setText("");
+      }, [listChats]);
+    console.log(listChats)
+
+    
 
     useEffect(() => {
         fetch("/messages.json")
@@ -42,7 +73,7 @@ export const ChatProvider = ({ children }: { children: React.ReactNode }) => {
     const isCurrentUser = (sender: string) => sender === currentUser;
 
     return (
-        <ChatContext.Provider value={{ handleClose, isClose, messages, currentUser, isCurrentUser }}>
+        <ChatContext.Provider value={{ handleClose, isClose, messages, currentUser, isCurrentUser, updateText, handleAdd }}>
             {children}
         </ChatContext.Provider>
     )
