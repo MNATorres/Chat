24f6ac4810b685aaa93037fe59cb379fcc299be4
matias.torres <mtri21019@gmail.com>
2023-10-29 06:00:56 -0300

    add function to close user list

--- src/components/HeaderPage/ListUsers.tsx
+++ src/components/HeaderPage/ListUsers.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useRef } from "react";
 import { FaUserFriends } from "react-icons/fa";
 import { useChat } from "../../hooks/useChat";
 import { useUser } from "../../hooks/useUser";
@@ -7,9 +7,22 @@ import style from "./styles.module.scss";
 export default function ListUsers() {
   const { handleCloseListUsers, closeListUsers } = useChat();
   const { setLoggedUser, usersList } = useUser();
+  const listUsersRef = useRef<any>(null);
+
+  useEffect(() => {
+    const handleOutsideClick = (e: { target: any }) => {
+      if (listUsersRef.current && !listUsersRef.current.contains(e.target)) {
+        handleCloseListUsers();
+      }
+    };
+    document.addEventListener("mousedown", handleOutsideClick);
+    return () => {
+      document.removeEventListener("mousedown", handleOutsideClick);
+    };
+  }, []);
 
   return (
-    <div className={style.listUsers}>
+    <div ref={listUsersRef} className={style.listUsers}>
       <div className={style.flexMovil}>
         <button onClick={handleCloseListUsers}>
           <FaUserFriends />
@@ -23,7 +36,13 @@ export default function ListUsers() {
         <ul className={style.ulChatHeader}>
           {usersList?.map((user) => {
             return (
-              <li key={user._id} onClick={() => setLoggedUser(user.user)}>
+              <li
+                key={user._id}
+                onClick={() => {
+                  setLoggedUser(user.user);
+                  handleCloseListUsers();
+                }}
+              >
                 {user.user}
               </li>
             );
